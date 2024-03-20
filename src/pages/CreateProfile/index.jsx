17bb22c0a73a813fb3fe33e9f60fe9f98b521263
jovijovi-john <jovijovi2019@hotmail.com>
2024-03-20 08:49:38 -0300

    feat: checkbox createProfile

--- src/pages/CreateProfile/index.jsx
+++ src/pages/CreateProfile/index.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import Page from '../../components/Page'
-import Header from '../../components/Header'
+import React, { useState } from 'react';
+import Page from '../../components/Page';
+import Header from '../../components/Header';
 
 export default function CreateProfile() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      setIsChecked(!isChecked);
+    }
+  };
+
   return (
     <Page>
       <Header title='Criando seu Perfil Smart TV' />
@@ -10,10 +18,21 @@ export default function CreateProfile() {
       <main>
         <input type="text" />
         <div className='text-white'>
-          <label><input type="checkbox" /> Este é um perfil de grupo (ex: Família, Hóspedes...)</label>
+          <label>
+            <input
+              type="checkbox"
+              id='teste'
+              name='teste'
+              value={'teste'}
+              checked={isChecked}
+              onKeyDown={handleKeyPress}
+              onChange={() => setIsChecked(!isChecked)}
+            />
+            Este é um perfil de grupo (ex: Família, Hóspedes...)
+          </label>
         </div>
         <input type="text" />
       </main>
     </Page>
-  )
+  );
 }
